49245416ba9b77f23a05979c5b596da946dda736
sofiasbrinas <sofiasabrinasilva@gmail.com>
2026-06-30 16:59:48 -0300

    docs: adiciona documentação interativa com Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Blog Pessoal')
+    .setDescription('Projeto Blog Pessoal')
+    .setContact(
+      'Sofia Sabrina',
+      'https://github.com/sofiasbrinas',
+      'sofiasabrinasilva@gmail.com',
+    )
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/docs', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
